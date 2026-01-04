e5a22f8c539a4322915f249e01b51bc0dcd6b199
Uminan <86194957+Uminan@users.noreply.github.com>
2026-01-04 16:24:32 +0000

    Deploying to gh-pages from @ Weiming-Research/weiming-research.github.io@fc6fd3d6bf0130e7b05a410336e1e1b5795d94d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,7 +458,7 @@ ninja.data = [{
               window.location.href = "/projects/9_project/";
             },},{id: "projects-radioformer",
           title: 'RadioFormer',
-          description: "RadioFormer: One Physics-Guided Transformer to Unify 2D and 3D Radio Map Construction.",
+          description: "RadioFormer: One Physics-Guided Transformer to Unify 2D and 3D Radio Map Construction. Radio maps, which model wireless signal quality across different locations, are essential for efficient network planning and positioning. This project aims to generate high-quality 2D and 3D radio maps based on environmental layouts. However, traditional deep learning approaches often fail to understand the underlying physical principles of signal propagation, making them ineffective in unknown environments. Additionally, existing 3D generation models are typically too slow for real-time application.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/radioformer/";
             },},{
