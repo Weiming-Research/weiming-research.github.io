ebb5e9011ae0234378812e87932f0993d72699e0
Uminan <86194957+Uminan@users.noreply.github.com>
2026-01-04 09:05:40 +0000

    Deploying to gh-pages from @ Weiming-Research/weiming-research.github.io@949d1c6aa8ce5dbc36d4af08ce8d789174536f46 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-title",
-          title: 'Project Title',
-          description: "A short description",
+          section: "News",},{id: "projects-radioformer",
+          title: 'RadioFormer',
+          description: "A unified physics-guided framework for real-time 2D/3D radio map construction with 2500× inference speedup.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
